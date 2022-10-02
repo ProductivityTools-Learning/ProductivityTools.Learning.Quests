b69d60636ca01ac3d643ded8834884039ce46a32
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-10-02 14:47:06 +0200

    reorg2

---
+++ Algorithms/DFS/ObjectDFS1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFS
+{
+    public class ObjectDFS1Worker
+    {
+        public void Do()
+        {
+            Node pawel = new Node() { Name = "Pawel" };
+            Node magda = new Node() { Name = "Magda" };
+            Node marcin = new Node() { Name = "Marcin" };
+            Node gosia = new Node() { Name = "Gosia" };
+            Node justyna = new Node() { Name = "Justyna" };
+
+            pawel.Nodes.Add(magda);
+            pawel.Nodes.Add(gosia);
+            magda.Nodes.Add(marcin);
+            magda.Nodes.Add(pawel);
+            gosia.Nodes.Add(pawel);
+            marcin.Nodes.Add(justyna);
+            marcin.Nodes.Add(magda);
+            justyna.Nodes.Add(marcin);
+
+            var dfs = new ObjectDFS1(pawel);
+            dfs.Search(pawel,"Gosia");
+
+
+            //dfs = new ObjectDFS1(justyna);
+            //result = dfs.Search("Pawel");
+            //Assert.IsNotNull(result);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Node
+    {
+        public string Name { get; set; }
+        public List<Node> Nodes { get; set; }
+
+        public Node()
+        {
+            this.Nodes = new List<Node>();
+        }
+    }
+
+    public class ObjectDFS1
+    {
+        Node FirstNode;
+        HashSet<Node> Visited = new HashSet<Node>();
+        Stack<Node> Path = new Stack<Node>();
+        public ObjectDFS1(Node firstNode)
+        {
+            this.FirstNode = firstNode;
+        }
+
+        public Node Search(Node node, string lookUpValue)
+        {
+            Visited.Add(node);
+            Path.Push(node);
+            if (node.Name == lookUpValue)
+            {
+                return node;
+            }
+            else
+            {
+                foreach (var childNode in node.Nodes)
+                {
+                    if (Visited.Contains(childNode) == false)
+                    {
+                        var result=Search(childNode, lookUpValue);
+                        if (result == null)
+                        {
+                            Path.Pop();
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
--- Algorithms/DFS/Program.cs
+++ Algorithms/DFS/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DFS;
+
 Console.WriteLine("Hello, World!");
+
+
+ObjectDFS1Worker dfs1=new ObjectDFS1Worker();
+dfs1.Do();
+
